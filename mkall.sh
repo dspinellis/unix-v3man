aabee733a600d0e4d1f2e56d4975c4ae0958d28c
Diomidis Spinellis <dds@aueb.gr>
2017-11-19 20:50:08 +0200

    Add PDF creation script

---
+++ mkall.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Typeset the complete Unix Programmer's Manual
+#
+# Diomidis Spinellis, November 2017
+#
+
+
+# Nroff to Postscript via enscript(1)
+psnroff()
+{
+  nroff -Tascii "$@" -P-c | enscript --margins=75 -l -p-
+}
+
+{
+  # Typeset the front matter
+  # No attempt is made to recreate the table of contents and index
+  (cd man0 && psnroff -me aa intro basinf toc)
+
+  # Typeset the individual manual pages
+  for s in man[1-8] ; do
+    for f in $s/* ; do
+      psnroff man0/aam $f
+    done
+  done
+} |
+# Convert all the Postscript output into PDF
+gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -sOutputFile=v3man.pdf -
